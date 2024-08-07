e73fb4a6e0b8143ebc3758cf54a1c9236912c7c5
Kamel Eltalib <91773519+KamelEltalib@users.noreply.github.com>
2024-08-07 10:48:01 +0300

    Delete v0.1 directory
    Delete v0.1 directory

--- v0.1/app/components/Header.js
+++
@@ -1,9 +0,0 @@
-const Header = `
-                  <header>
-                        <h1>مرحبا بك في Dakwa</h1>
-                        <p>أفضل منصة لتطوير التطبيقات الويب بسهولة وسرعة.</p>
-                        <a href="#features" class="cta-button">استكشف المزيد</a>
-                  </header>
-`
-
-export default Header
